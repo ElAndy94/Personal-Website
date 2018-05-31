8e1cc9edf5d3f8a4c5b01bf9af797b6b6a847a25
Andy <andrewpeliza@hotmail.com>
2018-05-31 23:56:36 +0100

    Education and Work experience added responsive!

--- src/app/education/education.component.ts
+++ src/app/education/education.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EducationComponent implements OnInit {
   yearThree = ['Project', 'Advanced Web Development', 'Data Engineering', 'Information Systems Strategy'];
+  yearTwo = ['Web Design and Development','Web Based Business Systems','Database Systems','Professional Development'];
+  yearOne = ['Java Programming','Computer System Fundamentals','Web and Multimedia','Information Systems and Database'];
+  college = 'Kingston College, London - Merit';
+  baysideSchool = 'Bayside Comprehensive, Gibraltar';
 
   public projectDescription = 'Research, design, implement, test and evaluate a JavaScript Game, made using Node.js, MongoDB, JavaScript and HTML5/CSS. Repository hosted on GitHub.';
 
@@ -15,6 +19,27 @@ export class EducationComponent implements OnInit {
   public dataEng = 'Research, analyse, test different methods of data mining technologies. Cleaned and made sense of a data set for mortgage applicants.';
 
   public infoSystems = 'Research different strategies and strategic planning in organisations, and how information systems and information technologies exploitation is central in supporting strategic goals.';
+
+  public webDesign = 'Design, implement, test a JavaScript Game, made using JavaScript, Node.js, SQL and HTML5/CSS. Create fully working website with user login system, online chat using JQuery and PHP.';
+
+  public webBased = 'Critically evaluate different technologies, use contemporary off-the-shelf e-commerce packages to sell goods, develop online and multi-channel marketing strategies, create web-based software for commerce with user tracking.';
+
+  public dataBase = 'Database Modelling (UML Diagrams and relational schemas). SQL Database implementation, relational algebra, physical aspects, query processing and optimisation, concurrency control and interfacing with programming languages and environments.';
+
+  public prof = 'Worked in a group to produce a fully working website and application for a small business. Group leader of the team assigning different roles to students.';
+
+  public javaProg = 'Intro to Java, and creating a space invaders shooting game.';
+
+  public webMulti = 'Principles of web design fundamentals with programs such as Photoshop and using CSS styling.';
+
+  public compFund = 'Assembly language and discrete mathematic theory.';
+
+  public infoSys = 'Production of an Oracle SQL database and learning database fundamentals.';
+
+  public gradeCollege = 'Access to Higher Education Computing.\n' +
+    'Selected for meetings with OCN London to give feedback on how to improve the access courses.';
+
+  public baySide = 'A*, B, C, Merit (Including English and Mathematics).'
 
   constructor() { }
 
--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-experience',
@@ -6,8 +6,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./experience.component.css']
 })
 export class ExperienceComponent implements OnInit {
+  workExp = ['(Summer Job) Customs Officer, Gibraltar', 'Managing Director of Andy Holdings Ltd', 'Co-Founder of Avanti Construction & Maintenance', 'ICT Technician, Systems Services, Gibraltar'];
 
-  constructor() { }
+  vol = 'Trainee at Gibraltar Fire & Rescue';
+
+  public cust = [
+    '• Signing important classified papers and checking merchandise, which gave me legal knowledge.', '• Dealing with various clients to sign off the clearing merchandise forms.', '• Clearing items and personal that cross the borders.', '• Keeping all old records up to date with the system.'];
+
+  public andy = [
+    '• Assisted the clients with refurbishments and other duties as required, working in line with health and safety.',
+    '• Supplied all deliveries to the clients personally which made me dependable and punctual.',
+    '• Managed my time for projects and their deadlines which helped me learn to work under pressure.',
+    '• Delivered high standards to exceed the customers’ needs.',
+    '• Dealt with various businesses involving high demands.'];
+
+  public avanti = [
+    '• Learnt the fundamentals of owning a business and the responsibilities associated.',
+    '• Read over contracts thoroughly and ensure that contract requirement is met.',
+    '• Made cost estimations for different types of refurbishments with various scales of projects.',
+    '• Lead a team of over fifteen employees which strongly improved my responsibility skills.'];
+
+  public ict = [
+    '• Installed and configured internet content filtering system for client businesses.',
+    '• Reinstalled software on computers for customers independently.',
+    '• Regularly took computers apart and rebuilt them for customers.',
+    '• Found and replaced defected component(s)'];
+
+  public volunt = [
+    '• This linked up with my past work experience at high school in which I also chose to learn about the Fire Brigade.',
+    '• Learnt the importance of a Fireman’s role.',
+    '• Trained on physical tasks such as abseiling.',
+    '• Learnt how to work under pressure in advanced rescue skills.',
+    '• Quick learner to adapt to every different tests and training with different scenarios daily.'];
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
